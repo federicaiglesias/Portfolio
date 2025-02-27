8f531d4bbc8133470b3ab0f793751d4b66e9fab2
federicaiglesias <federicaiglesias1@gmail.com>
2025-02-27 18:27:04 +0100

    updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/federicaiglesias"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -34,7 +34,7 @@ function Footer() {
 
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://www.linkedin.com/in/federica-iglesias-inciarte-2a949323a/"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
