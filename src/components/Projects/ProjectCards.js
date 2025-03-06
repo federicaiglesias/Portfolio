836bac84cc9da610691d41bfdb3e0fbafe3ef7d3
federicaiglesias <federicaiglesias1@gmail.com>
2025-03-06 11:27:39 +0100

    Update ProjectCards.js

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -6,35 +6,41 @@ import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
   return (
-    <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
-        </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
+    <>
+      <Card
+        className="project-card-view d-flex flex-column"
+        style={{ height: "100%" }}
+      >
+        <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+        <Card.Body className="d-flex flex-column" style={{ flexGrow: 1 }}>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text style={{ textAlign: "justify" }}>
+            {props.description}
+          </Card.Text>
+          <div className="mt-auto">
+            <Button variant="primary" href={props.ghLink} target="_blank">
+              <BsGithub /> &nbsp;
+              {props.isBlog ? "Blog" : "GitHub"}
+            </Button>
+            {"\n"}
+            {"\n"}
 
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
-          </Button>
-        )}
-      </Card.Body>
-    </Card>
+            {!props.isBlog && props.demoLink && (
+              <Button
+                variant="primary"
+                href={props.demoLink}
+                target="_blank"
+                style={{ marginLeft: "10px" }}
+              >
+                <CgWebsite /> &nbsp;
+                {"Demo"}
+              </Button>
+            )}
+          </div>
+        </Card.Body>
+      </Card>
+    </>
   );
 }
+
 export default ProjectCards;
